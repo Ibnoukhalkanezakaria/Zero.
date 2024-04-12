52aa3361ea510b68143f7787a5b8a773da7c222e
ahmedelqori <aelqori1@gmail.com>
2024-04-12 21:55:26 +0000

    test

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -2,23 +2,23 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import HomeCnt from "../../components/Home/HomeCnt";
 
+axios.defaults.withCredentials = true;
+
 const Home = () => {
   const [user, setUser] = useState(null);
   const [error, setError] = useState(null);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Fetching the refresh token
         const refreshTokenResponse = await axios.post(
           "http://localhost:3001/api/auth/refresh"
         );
-        const refreshToken = refreshTokenResponse.data.refreshToken;
-        // Fetching the user data using the access token
+        const accessToken = refreshTokenResponse.data.accessToken;
         const accessTokenResponse = await axios.get(
           "http://localhost:3001/api/auth/home",
           {
             headers: {
-              Authorization: `Bearer ${refreshToken}`,
+              Authorization: `Bearer ${accessToken}`,
             },
           }
         );
@@ -31,9 +31,8 @@ const Home = () => {
   }, []);
   return (
     <>
-      {/* {error && <div>{error}</div>} */}
-      {/* {user && <HomeCnt user={user} />} */}
-      <HomeCnt />
+      {error && <div>{error}</div>}
+      {user && <HomeCnt user={user} />}
     </>
   );
 };
